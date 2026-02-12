4056a30ca610619678f484c13d90b9f147e74818
Kelvin Giovanno <kelvingiovanno2000@gmail.com>
2026-02-12 23:29:46 +0700

    add eslint configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js  from "@eslint/js";
+import tseslint from "typescript-eslint"
+import globals from "globals";
+
+export default defineConfig([
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            globals: globals.browser,
+        },
+    },
+    js.configs.recommended,
+    tseslint.configs.recommended,
+    globalIgnores(["dist", "node_modules"])
+])
